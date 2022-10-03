9981ec0589762606ad4942eb4370808b961459b5
Yuberley <yuberleyguerrero1999@gmail.com>
2022-10-02 20:27:29 -0500

    finished basic practice

--- Django-Practice/myapp/models.py
+++ Django-Practice/myapp/models.py
@@ -1,3 +1,19 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technology = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ' - ' + self.project.title + ' : ' + self.description
--- Django-Practice/myapp/views.py
+++ Django-Practice/myapp/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from cmath import phase
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('Hello world')
+def index(request):
+    sentence = 'Sentence from views.py'
+    return render(request, 'index.html', {
+        'sentence': sentence,
+    })
+
+
+def hello(request, username):
+    return HttpResponse(f'Hello {username}')
+
+
+def about(request):
+    return HttpResponse('About')
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def task(request, id):
+    tasks = get_object_or_404(Task, pk=id)
+    return JsonResponse(tasks.name, safe=False)
+
+def tasks(request):
+    tasks = list(Task.objects.values())
+    return JsonResponse(tasks, safe=False)
